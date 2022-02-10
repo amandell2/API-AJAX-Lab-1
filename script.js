6de11a32ed72ec423b5643f4646e764c77dc5fef
Amandell <amanda.mandell2@gmail.com>
2022-02-09 22:00:43 -0500

    Completed extended challenges

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-const redditPromise = fetch("https://www.reddit.com/r/aww/.json");
+let submit = document.getElementById("theForm");
+
+//event listener that displays subreddit given by user
+submit.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const userInput = document.getElementById("userInput");
+    console.log(userInput.value);
+    const title = document.getElementById("title");
+    title.innerText = userInput.value + " " + "Subreddit Posts";
+
+//changed the API that is being fetched based on user input from above event listener
+const redditPromise = fetch("https://www.reddit.com/r/"+ userInput.value + "/.json");
 redditPromise.then(response => response.json())
 .then(data =>{
-    //console.log(data);
-    const articleContainer = document.getElementById("articleContainer")
-    for(let child of data.data.children){
+    //remove previous article container
+    const articleContainer = document.getElementById("articleContainer");
+    if(articleContainer){
+        articleContainer.remove();
+    };
+    //add new article container
+    const newContainer = document.createElement("div");
+    newContainer.setAttribute("id", "articleContainer");
+    document.body.append(newContainer);
+    
+    //Extended Challenge-only show first 10 results
+    let newArray = data.data.children.slice(0,10);
+    //console.log(newArray);
+
+    //appending subreddit items to page
+    for(let item of newArray){
         let newArticle = document.createElement('div');
         newArticle.setAttribute("class", "article")
         let newTitle = document.createElement('h2');
-        newTitle.innerText = child.data.title;
+        newTitle.innerText = item.data.title;
         let newImage = document.createElement('img')
         newImage.setAttribute("class","newImage");
-        newImage.src = child.data.thumbnail;
+        newImage.src = item.data.thumbnail;
         let newLink = document.createElement("a");
-        newLink.setAttribute("href", "https://www.reddit.com"+child.data.permalink);
+        newLink.setAttribute("href", "https://www.reddit.com"+item.data.permalink);
         newLink.innerText = "Check out full article here!";
         newArticle.append(newTitle);
         newArticle.append(newImage);
         newArticle.append(newLink);
-        articleContainer.append(newArticle);
+        newContainer.append(newArticle);
     }
-    //Manipulaate and create HTML elements
+});
 });
